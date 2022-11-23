299bb08c06db98067e95423c254d904edb282612
ismail <ismail@ISMAILBARISOZTU>
2022-11-23 13:32:08 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramerwork/EfProductDal.cs
@@ -0,0 +1,63 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramerwork
+{
+    public class EfProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            //IDısposable pattern implemantation of c#
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var addedEntity = context.Entry(entity); //referansı yakala
+                addedEntity.State = EntityState.Added;  //eklenecek nesne
+                context.SaveChanges();   //ekle
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity); //referansı yakala
+                deletedEntity.State = EntityState.Deleted;  //eklenecek nesne
+                context.SaveChanges();   //ekle
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NorthwindContext context=new NorthwindContext())
+            {
+                return filter == null 
+                    ? context.Set<Product>().ToList() //filtre null ise bu
+                    : context.Set<Product>().Where(filter).ToList();     //null değil ise bu çalışır
+            }
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var updatedEntity = context.Entry(entity); //referansı yakala
+                updatedEntity.State = EntityState.Modified;  //eklenecek nesne
+                context.SaveChanges();   //ekle
+            }
+        }
+    }
+}
